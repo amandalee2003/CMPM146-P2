b2af33ffe2a4ce4605ff5da1d64b9a911ed45957
pdobbins23 <146784496+pdobbins23@users.noreply.github.com>
2024-07-06 14:01:25 -0700

    Added notes for mcts_vanilla

--- src/mcts_vanilla.py
+++ src/mcts_vanilla.py
@@ -7,6 +7,8 @@ from math import sqrt, log
 num_nodes = 100
 explore_faction = 2.
 
+# Selection Stage
+# The goal here is to traverse down the most interesting/optimal path until we reach a leaf to expand
 def traverse_nodes(node: MCTSNode, board: Board, state, bot_identity: int):
     """ Traverses the tree until the end criterion are met.
     e.g. find the best expandable node (node with untried action) if it exist,
@@ -25,6 +27,11 @@ def traverse_nodes(node: MCTSNode, board: Board, state, bot_identity: int):
     """
     pass
 
+# Expansion Stage
+# Here we generate random legal moves and pick one to simulate
+# For vanilla MCTS, I think this just means completely random moves,
+# but for modified MCTS we can use a heuristic to estimate optimal
+# moves
 def expand_leaf(node: MCTSNode, board: Board, state):
     """ Adds a new leaf to the tree by creating a new child node for the given node (if it is non-terminal).
 
@@ -40,7 +47,9 @@ def expand_leaf(node: MCTSNode, board: Board, state):
     """
     pass
 
-
+# Simulation Stage
+# Here we randomly chose legal moves for both opponents until we reach an end state (win, lose, draw)
+# Then we return the score which I think would be (1, -1, 0) respectively
 def rollout(board: Board, state):
     """ Given the state of the game, the rollout plays out the remainder randomly.
 
@@ -54,7 +63,9 @@ def rollout(board: Board, state):
     """
     pass
 
-
+# Backpropagation Stage
+# This is just walking back up the tree to the root, incrementing the win/play values of each node as it passes by them
+# Im not sure if we should consider a draw as a loss or not
 def backpropagate(node: MCTSNode|None, won: bool):
     """ Navigates the tree from a leaf node to the root, updating the win and visit count of each node along the path.
 
@@ -65,6 +76,11 @@ def backpropagate(node: MCTSNode|None, won: bool):
     """
     pass
 
+# Upper Confidence Bound (https://en.wikipedia.org/wiki/Thompson_sampling#Upper-Confidence-Bound_(UCB)_Algorithms)
+# Formula for MCTS: w / n + c * sqrt(ln(p) / n)
+# where w = total wins for current node, n = visit count for current node, p = visit count for parent node, c = constant that controls the rate of exploration (usually set to sqrt(2))
+# w / n = the average win rate of the node, which makes the algorithm favor well performing paths
+# c * sqrt(ln(p) / n) = represents urge to explore, gets smaller as node is explored more, so pushes the algorithm to explore less explored nodes
 def ucb(node: MCTSNode, is_opponent: bool):
     """ Calcualtes the UCB value for the given node from the perspective of the bot
 
@@ -76,6 +92,7 @@ def ucb(node: MCTSNode, is_opponent: bool):
     """
     pass
 
+# This should pick which child node has the best likely outcome/path
 def get_best_action(root_node: MCTSNode):
     """ Selects the best action from the root node in the MCTS tree
 
